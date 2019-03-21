c295f85a5018d829acb50c5ad971793f72e5a70a
Taoufiq-Imad <Taoufiq-Imad@users.noreply.github.com>
2019-03-21 21:29:46 +0100

    affichage des users

---
+++ js/users.js
@@ -0,0 +1,298 @@
+var users =[
+    {
+        "gender": "female",
+        "name": {
+            "title": "miss",
+            "first": "léa",
+            "last": "roger"
+        },
+        "email": "léa.roger@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/25.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/25.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/25.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "miss",
+            "first": "océane",
+            "last": "andre"
+        },
+        "email": "océane.andre@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/4.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/4.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/4.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "miss",
+            "first": "lilou",
+            "last": "robin"
+        },
+        "email": "lilou.robin@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/48.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/48.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/48.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "soan",
+            "last": "berger"
+        },
+        "email": "soan.berger@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/58.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/58.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/58.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "leana",
+            "last": "rousseau"
+        },
+        "email": "leana.rousseau@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/50.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/50.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/50.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "ethan",
+            "last": "deschamps"
+        },
+        "email": "ethan.deschamps@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/0.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/0.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/0.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "emmy",
+            "last": "martin"
+        },
+        "email": "emmy.martin@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/68.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/68.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/68.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "marin",
+            "last": "bernard"
+        },
+        "email": "marin.bernard@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/53.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/53.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/53.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "ms",
+            "first": "séléna",
+            "last": "leclerc"
+        },
+        "email": "séléna.leclerc@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/47.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/47.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/47.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "amelia",
+            "last": "pierre"
+        },
+        "email": "amelia.pierre@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/34.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/34.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/34.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "alexandre",
+            "last": "hubert"
+        },
+        "email": "alexandre.hubert@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/33.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/33.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/33.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "elias",
+            "last": "robert"
+        },
+        "email": "elias.robert@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/45.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/45.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/45.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "milan",
+            "last": "gauthier"
+        },
+        "email": "milan.gauthier@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/66.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/66.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/66.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "nino",
+            "last": "legrand"
+        },
+        "email": "nino.legrand@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/51.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/51.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/51.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "noham",
+            "last": "nguyen"
+        },
+        "email": "noham.nguyen@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/99.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/99.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/99.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "louane",
+            "last": "noel"
+        },
+        "email": "louane.noel@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/32.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/32.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/32.jpg"
+        }
+    }
+]
+
+
+let getallusers= ()=>{
+  
+                let divusers=document.querySelector('.row')
+                divusers.innerHTML=""
+                for (const user of users) {
+                    let divuser=  `<div class="col-12 col-md-6 col-lg-3 margin-top">
+                    <div class="card " style="width: 100%;">
+                        <div class="row">
+                            <div class="col-5 col-md-12">
+                        <img src="${user.picture.large}" class="card-img-top" alt="...">
+                         </div>
+                        <div class="card-body col-7 <!-- En-tête du tableau --> col-md-12 ">
+                        <h5 class="card-title">${user.name.title}.${user.name.last} ${user.name.first}</h5>
+                        <p class="card-text">${user.email}</p>
+                        <button href="#" class="btn btn-primary">Go somewhere</button>
+                        </div>
+                    </div>
+                </div>
+                </div>` 
+                divusers.innerHTML+=divuser
+                }
+                    
+
+}
+
+    getallusers()
+ 
+
+let totabl=()=>{
+    let divtab=document.querySelector('.row')
+    let html=`<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>prenom</th>
+            <th>sexe</th>
+            <th>mail</th>
+        </tr>
+    </thead>
+ 
+    <tbody>`
+
+    for (const user of users) {
+        html+=`<tr>
+        <td>${user.name.last}</td>
+        <td>${user.name.first}</td>
+        <td>${user.gender}</td>
+        <td>${user.email}</td>
+    </tr>`
+    }
+    html+=`</tbody>
+    </table>`
+    divtab.innerHTML=html;
+}
+
+let tab=document.querySelector('#totab')
+tab.addEventListener('click',e=>{
+    e.preventDefault()
+    if (tab.name==1) {
+        tab.name=0
+        tab.innerText="precedant"
+        totabl()
+        
+    }else{
+        tab.name=1
+        tab.innerText="affiche form tableau"
+        getallusers()
+    }
+    
+})
